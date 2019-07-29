31b6e0a338298f49361ab335969ee3363f8b956d
Pasan Godamune <pasanjg@gmail.com>
2019-07-29 23:33:50 +0530

    project init

---
+++ src/main/java/com/sliit/spm/SpmCodexSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.spm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmCodexSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmCodexSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit/spm/SpmCodexSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sliit.spm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpmCodexSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
