3d8cd55ec704814d7f94985b16c51123261700b5
ShalikaAshan01 <shalikaashan01@gmail.com>
2019-08-09 15:50:50 +0530

    function completed

--- src/main/java/com/sliit/spm/controller/Measure.java
+++ src/main/java/com/sliit/spm/controller/Measure.java
@@ -1,5 +1,8 @@
 package com.sliit.spm.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,21 +17,31 @@ public class Measure {
     private int currentChar;
     private int prevChar;
     private String word;
-    private String prevDetectLogic;
-    private String prevDetectControlStruc;
+    private String line;
+    private String detectedKeyword;
+    private String prevDetectedKeyword;
     private int count;
     private int weight;
     private String file;
     private FileReader fileReader;
+    private JSONObject json;
+    private JSONObject tempJSON;
+    private JSONArray jsonArray;
+    private JSONArray tempArr;
 
     public Measure(String file){
         this.file = file;
         prevChar=0;
         word = "";
-        prevDetectLogic = "";
-        prevDetectControlStruc = "";
-        count =0;
+        line = "";
+        detectedKeyword = "";
+        prevDetectedKeyword = "";
+        count =1;
         weight = 0;
+        json = new JSONObject();
+        tempJSON = new JSONObject();
+        jsonArray = new JSONArray();
+        tempArr = new JSONArray();
         try {
             fileReader = new FileReader(System.getProperty("user.dir")+"/temp/" + this.file);
         } catch (FileNotFoundException e) {
@@ -49,49 +62,116 @@ public class Measure {
         if (word.contains("if")){
             weight++;
             word = "";
-            prevDetectControlStruc = "if";
+            detectedKeyword = "if";
+            jsonArray.put("if");
         }
     }
 
     public void detectLogicalOperator(int increment){
         if (currentChar == LOGICAL_AND) {
             if (prevChar != LOGICAL_AND) {
-                prevDetectLogic = "&";
                 word = "";
                 weight += increment;
+                jsonArray.put("AND");
             } else {
                 word = "";
-                prevDetectLogic = "&&";
             }
-        }
-
-        if (currentChar == LOGICAL_OR) {
+        }else if (currentChar == LOGICAL_OR) {
             if (prevChar != LOGICAL_OR) {
-                prevDetectLogic = "|";
                 word = "";
                 weight+=increment;
+                jsonArray.put("OR");
             } else {
                 word = "";
-                prevDetectLogic = "||";
             }
         }
     }
 
     public void detectBracketClose(){
         if(currentChar==BRACKET_CLOSE){
-            prevDetectControlStruc = "";
-            prevDetectLogic = "";
+            prevDetectedKeyword =detectedKeyword;
+            detectedKeyword = "";
+//            if(prevDetectLogic != null && !prevDetectLogic.equals("")){
+//                jsonArray.put(prevDetectLogic);
+//                prevDetectLogic=null;
+//            }
+        }
+    }
+
+    public void detectIterativeControlStructure(){
+        /*
+        detect for loop
+         */
+        if(word.contains("for")){
+            weight += 2;
+            word = "";
+            detectedKeyword = "for";
+            jsonArray.put("for");
+        }
+        if(word.contains("do")){
+            weight += 2;
+            word = "";
+            detectedKeyword = "do";
+            jsonArray.put("do-while");
+        }
+        /*
+        detect while loop
+         */
+        if(word.contains("while") && (!detectedKeyword.contains("do") && (!prevDetectedKeyword.contains("do")))){
+            weight += 2;
+            word = "";
+            detectedKeyword = "while";
+            jsonArray.put("while");
+        }
+    }
+
+    public void detectCatchStatement(){
+        if(word.contains("catch")){
+            weight += 1;
+            word = "";
+            detectedKeyword = "catch";
+            jsonArray.put("catch");
+        }
+    }
+
+    public void detectSwitch(){
+        if(word.contains("switch")){
+            detectedKeyword="switch";
+            word = "";
         }
     }
 
     public void measureCtC(){
         while ((currentChar=readChar())!=-1){
+
+            line = line.concat(Character.toString((char) currentChar));
+            if(currentChar==10){
+                tempJSON.put("no",count);
+                tempJSON.put("line",line.replace("  ",""));
+                line = "";
+                tempJSON.put("tokens",jsonArray);
+                tempArr.put(tempJSON);
+                tempJSON = new JSONObject();
+                jsonArray = new JSONArray();
+                count++;
+            }
+
             if(this.currentChar != SPACE){
                 word = word.concat(Character.toString((char) currentChar));
                 detectConditionalControlStructure();
                 detectBracketClose();
-                if(prevDetectControlStruc.equals("if")){
+                if(detectedKeyword.equals("if")){
                     detectLogicalOperator(1);
+                }
+                detectIterativeControlStructure();
+
+                if(detectedKeyword.equals("while") || detectedKeyword.equals("for") || detectedKeyword.equals("do"))
+                    detectLogicalOperator(2);
+                detectCatchStatement();
+                detectSwitch();
+                if(prevDetectedKeyword.equals("switch") && word.contains("case")) {
+                    jsonArray.put("case");
+                    weight++;
                 }
 
             }
@@ -101,7 +181,10 @@ public class Measure {
         }
     }
 
-    public int getWeight() {
-        return weight;
+    public String get() {
+        json = new JSONObject();
+        json.put("code",tempArr);
+        json.put("weight",weight);
+        return json.toString();
     }
 }
--- src/main/java/com/sliit/spm/controller/MeasureComplexity.java
+++ src/main/java/com/sliit/spm/controller/MeasureComplexity.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/measure")
@@ -18,11 +14,11 @@ public class MeasureComplexity {
 //    5d4822e8ada9aa1a243d5d80 FibonacciMain
 //    5d482360ada9aa1a243d5d82 myException file
     @GetMapping("/controlStructure/{fileName}")
-    public int calculateCnC(@PathVariable String fileName){
+    public String calculateCnC(@PathVariable String fileName){
 
         Measure measure = new Measure(fileName);
         measure.measureCtC();
-        return measure.getWeight();
+        return measure.get();
     }
 }
 
